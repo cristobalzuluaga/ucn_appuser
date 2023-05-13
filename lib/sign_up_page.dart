12fe690a66efa31abbdc803569b7c06a1c12be83
unknown <leidyestrada2012@gmail.com>
2023-05-13 15:50:48 -0500

    limpia campos despues de guardar o borrar

--- lib/sign_up_page.dart
+++ lib/sign_up_page.dart
@@ -1,89 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'user_model.dart';
-
-
-
-// class SignUpPage extends StatefulWidget {
-//   const SignUpPage({Key? key}) : super(key: key);
-
-//   @override
-//   _SignUpPageState createState() => _SignUpPageState();
-// }
-
-// class _SignUpPageState extends State<SignUpPage> {
-//   final _formKey = GlobalKey<FormState>();
-//   final _usernameController = TextEditingController();
-//   final _emailController = TextEditingController();
-//   final _ageController = TextEditingController();
-
-//   List<User> users = [];
-  
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Sign Up'),
-//       ),
-//       body: Form(
-//         key: _formKey,
-//         child: Column(
-//           children: <Widget>[
-//             //Image.network('https://www.comfenalcoantioquia.com.co/wcm/connect/11733b3f-6812-4dbe-a616-77f36c98f664/1/49.png?MOD=AJPERES'),
-//             TextFormField(
-//               controller: _usernameController,
-//               decoration: const InputDecoration(labelText: 'Username'),
-//               validator: (value) {
-//                 if (value == null || value.isEmpty) {
-//                   return 'Please enter your username';
-//                 }
-//                 return null;
-//               },
-//             ),
-//             TextFormField(
-//               controller: _emailController,
-//               decoration: const InputDecoration(labelText: 'Email'),
-//               validator: (value) {
-//                 if (value == null || value.isEmpty) {
-//                   return 'Please enter your email';
-//                 }
-//                 if (!RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-//                     .hasMatch(value)) {
-//                   return 'Please enter a valid email';
-//                 }
-//                 return null;
-//               },
-//             ),
-//             TextFormField(
-//               controller: _ageController,
-//               decoration: const InputDecoration(labelText: 'Age'),
-//               validator: (value) {
-//                 if (value == null || value.isEmpty) {
-//                   return 'Please enter your age';
-//                 }
-//                 return null;
-//               },
-//             ),
-//             ElevatedButton(
-//               onPressed: () {
-//                 if (_formKey.currentState!.validate()) {
-//                   users.add(User(
-//                       username: _usernameController.text,
-//                       email: _emailController.text,
-//                       age: _ageController.text));
-//                   ScaffoldMessenger.of(context).showSnackBar(
-//                     const SnackBar(content: Text('User Registered')),
-//                   );
-//                 }
-//               },
-//               child: const Text('Submit'),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'user_model.dart';
 
@@ -164,6 +78,9 @@ class _SignUpPageState extends State<SignUpPage> {
                       ScaffoldMessenger.of(context).showSnackBar(
                         const SnackBar(content: Text('User Registered')),
                       );
+                      _usernameController.clear();
+                    _emailController.clear();
+                    _ageController.clear();
                     }
                   },
                   child: const Text('Submit'),
@@ -177,6 +94,9 @@ class _SignUpPageState extends State<SignUpPage> {
                     ScaffoldMessenger.of(context).showSnackBar(
                       const SnackBar(content: Text('User Deleted')),
                     );
+                    _usernameController.clear();
+                    _emailController.clear();
+                    _ageController.clear();
                   },
                   child: const Text('Delete'),
                 ),
